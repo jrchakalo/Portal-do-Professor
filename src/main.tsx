848f97dabd82903f7b2228f5adc8cce55d775926
jrchakalo <jisj@cin.ufpe.br>
2025-11-09 08:18:05 -0300

    feat(styles): integrate chakra provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { ChakraProvider } from '@chakra-ui/react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import './index.css';
+
+import App from './App.tsx';
+import { system } from './styles/theme';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ChakraProvider value={system}>
+      <App />
+    </ChakraProvider>
   </StrictMode>,
-)
+);
